9da594fb21240cf8d169d84e452cf2bce1037bf9
Lucas Pearson <lpearson@trek10.com>
2022-04-18 19:11:51 -0400

    Add test coverage

--- clickup/clickup.go
+++ clickup/clickup.go
@@ -1,12 +1,16 @@
 package clickup
 
 import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 const (
-	BASE_URL = "https://app.clickup/api/v2/"
+	BASE_URL = "https://app.clickup/api/v2"
 )
 
 var defaultHeaders = map[string]string{
@@ -27,7 +31,13 @@ func NewClient(httpClient *http.Client) (*Client, error) {
 		httpClient = http.DefaultClient
 	}
 
-	client := &Client{httpClient: httpClient}
+	baseURL, err := url.Parse(BASE_URL)
+
+	if err != nil {
+		return &Client{}, err
+	}
+
+	client := &Client{httpClient: httpClient, baseURL: baseURL}
 	client.headers = defaultHeaders
 	return client, nil
 }
@@ -39,4 +49,84 @@ func (c *Client) SetHeader(key string, value string) {
 
 func (c *Client) SetCredential(pk string) {
 	c.credential = pk
+}
+
+// includeHeaders set HTTP headers from client.headers to *http.Request
+func (c *Client) includeHeaders(req *http.Request) {
+	for key, value := range c.headers {
+		req.Header.Set(key, value)
+	}
+}
+
+func (c *Client) prepareRequest(ctx context.Context, req *http.Request) *http.Request {
+	out := req.WithContext(ctx)
+	c.includeHeaders(out)
+	if c.credential != "" {
+		// throw an error
+	}
+
+	return out
+}
+
+// get get JSON data from API and returns its body as []bytes
+func (c *Client) get(ctx context.Context, path string) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodGet, c.baseURL.String()+path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req = c.prepareRequest(ctx, req)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, Error{
+			body: body,
+			resp: resp,
+		}
+	}
+	return body, nil
+}
+
+func (c *Client) post(ctx context.Context, path string, data interface{}) ([]byte, error) {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, c.baseURL.String()+path, strings.NewReader(string(bytes)))
+	if err != nil {
+		return nil, err
+	}
+
+	req = c.prepareRequest(ctx, req)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if !(resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusCreated) {
+		return nil, Error{
+			body: body,
+			resp: resp,
+		}
+	}
+
+	return body, nil
 }
--- clickup/clickup_test.go
+++ clickup/clickup_test.go
@@ -1,12 +1,19 @@
 package clickup
 
 import (
+	"net/http"
+	"strings"
 	"testing"
 )
 
 func TestNewClient(t *testing.T) {
-	if _, err := NewClient(nil); err != nil {
+	c, err := NewClient(nil)
+	if err != nil {
 		t.Fatal("Failed to create Client")
+	}
+
+	if c.baseURL.String() != BASE_URL {
+		t.Fatal("Failed to set base URL")
 	}
 }
 
@@ -25,5 +32,38 @@ func TestSetCredential(t *testing.T) {
 
 	if client.credential != "pk_test" {
 		t.Fatal("client.credential returns wrong key: " + client.credential)
+	}
+}
+
+func TestIncludeHeaders(t *testing.T) {
+	client, _ := NewClient(nil)
+	client.headers = map[string]string{
+		"Header1":      "1",
+		"Header2":      "2",
+		"Content-Type": "application/json",
+	}
+
+	req, _ := http.NewRequest("POST", "localhost", strings.NewReader(""))
+	client.includeHeaders(req)
+
+	if len(req.Header) != 3 {
+		t.Fatal("req.Header length does not match")
+	}
+
+	for k, v := range req.Header {
+		switch k {
+		case "Header1":
+			if v[0] != "1" {
+				t.Fatalf(`%s header expect "1", but got "%s"`, k, v[0])
+			}
+		case "Header2":
+			if v[0] != "2" {
+				t.Fatalf(`%s header expect "2", but got "%s"`, k, v[0])
+			}
+		case "Content-Type":
+			if v[0] != "application/json" {
+				t.Fatalf(`%s header expect "2", but got "%s"`, k, v[0])
+			}
+		}
 	}
 }
